2753ebac3d1e1e10c562d11cef3dfac3b0dbc829
Smorodin Stanislav <stasvaganov@gmail.com>
2023-09-05 10:17:59 +0200

    initial commit

---
+++ 01_HTML_05_09_2023/script.js
@@ -0,0 +1,25 @@
+const bankAccount = {
+    accountNumber: "123456789",
+    accountHolderName: "Alice",
+    balance: 1000,
+    deposit: function (amount) {
+        amount > 60000
+            ? console.log("Требуется подтверждения легальности дохода")
+            : (this.balance += amount);
+    },
+    withdraw(amount) {
+        amount <= this.balance && amount > 0
+            ? (this.balance -= amount)
+            : console.log("Недостаточно средств на вашем балансе или вы ввели отрицательно число");
+    },
+    checkBalance() {
+        console.log(`Балансе вашего счета равен: ${this.balance}`)
+    }
+}
+bankAccount.checkBalance();
+bankAccount.deposit(500);
+bankAccount.checkBalance();
+bankAccount.withdraw(700);
+bankAccount.checkBalance();
+bankAccount.withdraw(1000);
+bankAccount.checkBalance();
